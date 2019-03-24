b44047f6dd388945ed7658b400e012780e849156
David Cressman <dac9xw@virginia.edu>
2019-03-24 13:24:47 -0400

    update

---
+++ arrayToString.js
@@ -0,0 +1,5 @@
+var favTeams = ["Manchester United", "Virginia", "Phillies", "Sixers"];
+function printFavs (array){
+    for (var i = 0; i < favTeams.length; i++) {
+        console.log(favTeams[i]);}
+}
